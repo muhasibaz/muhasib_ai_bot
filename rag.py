8376405094e53cbd4e4f158886808f037cd1a594
muhasibaz <muhasibaz@gmail.com>
2026-04-18 16:30:10 +0400

    Create rag.py

---
+++ rag.py
@@ -0,0 +1,22 @@
+import chromadb
+from chromadb.utils import embedding_functions
+from openai import OpenAI
+
+client = OpenAI()
+
+chroma_client = chromadb.Client()
+
+collection = chroma_client.create_collection(name="laws")
+
+def add_document(text, doc_id):
+    collection.add(
+        documents=[text],
+        ids=[doc_id]
+    )
+
+def search_docs(query):
+    results = collection.query(
+        query_texts=[query],
+        n_results=3
+    )
+    return results["documents"][0]
